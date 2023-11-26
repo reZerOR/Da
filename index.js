8c223a0392bd0d35216b0a82f6074ed1cfb0975f
reZerOR <gkraihan14k@gmail.com>
2023-11-26 23:12:05 +0600

    database connnection and user db created

--- index.js
+++ index.js
@@ -2,12 +2,60 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // ready made middleware
 app.use(cors());
 app.use(express.json())
 
+// database
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4gj9vp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // COLLECTIONS
+    const userCollection = client.db("dataDumpDB").collection("users");
+
+
+
+    // USER APIS
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists: 
+     
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+    //   checking is email exits or not
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
